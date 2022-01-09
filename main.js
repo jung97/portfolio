e1a1a8bfefbc9e5854bff0ef54e576e2c537bfb8
jung97 <eprtms3@naver.com>
2022-01-10 01:00:36 +0900

    JS:Navbar meu click

--- main.js
+++ main.js
@@ -3,10 +3,26 @@
 // Make navbar transparent when it is on the top -> navbar 투명 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll' ,() => {
-    if(window.scrollY > navbarHeight) {
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView( {behavior: "smooth"});
+
+});
+
