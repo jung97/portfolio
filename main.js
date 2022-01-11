23b6093beb4e6439eb6f52afa4c10e58af78123b
jung97 <eprtms3@naver.com>
2022-01-11 17:08:51 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -37,6 +37,21 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+//show "arrow up" button when scrolling down
+const arrowup = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+   if(window.scrollY > homeHeight /2) {
+       arrowup.classList.add('visible');
+   }  else {
+       arrowup.classList.remove('visible');
+   }
+});
+
+// Handle click on the arrow up button
+arrowup.addEventListener('click', () =>{
+    scrollIntoView('#home');
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
